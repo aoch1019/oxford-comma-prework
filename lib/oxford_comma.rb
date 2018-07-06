b5d60ef6babe7a455fd00784ed09d0fb92f1d1d5
Andrew Och <andrewoch95@gmail.com>
2018-07-06 04:27:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,9 @@ def oxford_comma(array)
   when 1
     return array.push
   when 2
-    conv_string = "and " + array.pop
+    conv_string = " and " + array.pop
   else
-    conv_string = ",and " + array.pop
+    conv_string = " ,and " + array.pop
   end
   conv_string.prepend(array.join(", "))
   return conv_string
