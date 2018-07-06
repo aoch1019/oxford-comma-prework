6e0616f8c65daa1198ac4e4d09eea541e2afecd5
Andrew Och <andrewoch95@gmail.com>
2018-07-06 04:25:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 def oxford_comma(array)
   conv_string = ""
   case array.length
-  if array.length == 0
+  when 0
     return ""
-  elsif array.length == 1
-  elsif array.length == 2
+  when 1
+    return array.flatten
+  when 2
     conv_string = "and " + array.pop
-  elsif array.length == 1
+  else
     conv_string = ",and " + array.pop
-  
   end
   conv_string.prepend(array.join(", "))
   return conv_string
